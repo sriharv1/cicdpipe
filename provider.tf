32246d4d2bd50723981db26f7f5a5ea7d943d95f
hv024036 <sriharsha.vallabaneni@cerner.com>
2024-12-27 11:42:08 +0530

    modifying the terraform provider

--- provider.tf
+++ provider.tf
@@ -8,9 +8,9 @@ terraform {
 }
 
 provider "oci" {
-    tenancy_ocid = "ocid1.tenancy.oc1..aaaaaaaaxva6ybvrffehf32ricoahwwssq4mu4ecm2tr5552gfczt7oasfya"
-    user_ocid = "ocid1.user.oc1..aaaaaaaasiajqzxnirtf4t5snqtlwpofsipdurlpo6eb7yj5xjawaw5fsidq"
-    fingerprint = "d2:fb:ad:6b:a8:e5:81:74:38:bf:c3:6e:c3:dd:d2:b5"
-    region = "sa-saopaulo-1"
-    private_key_path = "/Users/sriharshavallabaneni/Desktop/Cerner - harsha/terraform/terraform_private.pem"
+    tenancy_ocid = "${TF_VAR_tenancy_ocid}"
+    user_ocid = "${TF_VAR_user_ocid}"
+    fingerprint = "${TF_VAR_fingerprint}"
+    region = "${TF_VAR_region}"
+    private_key_path = "./private_key.pem"
 }
